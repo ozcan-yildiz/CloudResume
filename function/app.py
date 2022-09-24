5eeb1e39832abd14b581132e46df1db8b33b31c4
ozcan-yildiz <111027001+ozcan-yildiz@users.noreply.github.com>
2022-09-24 02:25:14 +0100

    Add files via upload
    SAM Mod

---
+++ function/app.py
@@ -0,0 +1,33 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'counter-db1'
+
+def lambda_handler(event, context):
+    
+    '''
+    data = client.get_item(
+        TableName='counter-db1',
+        Key = {
+            'id': {'S': 'view-count'}
+        }
+    )
+    '''
+    
+    #data['Item']['Quantity']['N'] = str(int(data['Item']['Quantity']['N']) + 1)
+    
+    response = client.update_item(
+        TableName='counter-db1',
+        Key = {
+            'id': {'S': 'view-count'}
+        },
+        UpdateExpression = 'ADD Quantity :inc',
+        ExpressionAttributeValues = {":inc" : {"N": "1"}},
+        ReturnValues = 'UPDATED_NEW'
+        )
+        
+    value = response['Attributes']['Quantity']['N']
+    
+    return {      
+            'statusCode': 200,
+            'body': value}
